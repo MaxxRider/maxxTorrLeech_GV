a1c533f3c6c34dee4ae8792e1e8f217ee8a45923
MaxxRider2 <66148227+MaxxRider2@users.noreply.github.com>
2020-07-25 12:05:23 +0530

    Update status_message_fn.py

--- tobrot/plugins/status_message_fn.py
+++ tobrot/plugins/status_message_fn.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # (c) Shrimadhav U K | gautamajay52
-
+ 
 # the logging things
 import logging
 logging.basicConfig(
@@ -10,24 +10,25 @@ logging.basicConfig(
 )
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 LOGGER = logging.getLogger(__name__)
-
+ 
 import asyncio
 import os
 import time
 import sys
 import traceback
 import io
-
+ 
 from tobrot import (
-    MAX_MESSAGE_LENGTH
+    MAX_MESSAGE_LENGTH,
+    AUTH_CHANNEL
 )
-
-
+ 
+ 
 from tobrot.helper_funcs.admin_check import AdminCheck
 from tobrot.helper_funcs.download_aria_p_n import call_apropriate_function, aria_start
 from tobrot.helper_funcs.upload_to_tg import upload_to_tg
-
-
+ 
+ 
 async def status_message_f(client, message):
     if await AdminCheck(client, message.chat.id, message.from_user.id):
         aria_i_p = await aria_start()
@@ -73,7 +74,7 @@ async def status_message_f(client, message):
         if msg == "":
             msg = "🤷‍♂️ No Active, Queued or Paused TORRENTs"
         await message.reply_text(msg, quote=True)
-
+ 
 async def cancel_message_f(client, message):
     if len(message.command) > 1:
         # /cancel command
@@ -94,17 +95,17 @@ async def cancel_message_f(client, message):
             )
     else:
         await message.delete()
-
+ 
 async def exec_message_f(client, message):
-    if await AdminCheck(client, message.chat.id, message.from_user.id):
+    if message.from_user.id in AUTH_CHANNEL:
         DELAY_BETWEEN_EDITS = 0.3
         PROCESS_RUN_TIME = 100
         cmd = message.text.split(" ", maxsplit=1)[1]
-
+ 
         reply_to_id = message.message_id
         if message.reply_to_message:
             reply_to_id = message.reply_to_message.message_id
-
+ 
         start_time = time.time() + PROCESS_RUN_TIME
         process = await asyncio.create_subprocess_shell(
             cmd,
@@ -122,7 +123,7 @@ async def exec_message_f(client, message):
             _o = o.split("\n")
             o = "`\n".join(_o)
         OUTPUT = f"**QUERY:**\n__Command:__\n`{cmd}` \n__PID:__\n`{process.pid}`\n\n**stderr:** \n`{e}`\n**Output:**\n{o}"
-
+ 
         if len(OUTPUT) > MAX_MESSAGE_LENGTH:
             with open("exec.text", "w+", encoding="utf8") as out_file:
                 out_file.write(str(OUTPUT))
@@ -137,13 +138,13 @@ async def exec_message_f(client, message):
             await message.delete()
         else:
             await message.reply_text(OUTPUT)
-
-
+ 
+ 
 async def upload_document_f(client, message):
     imsegd = await message.reply_text(
         "processing ..."
     )
-    if await AdminCheck(client, message.chat.id, message.from_user.id):
+    if message.from_user.id in AUTH_CHANNEL:
         if " " in message.text:
             recvd_command, local_file_name = message.text.split(" ", 1)
             recvd_response = await upload_to_tg(
@@ -158,27 +159,27 @@ async def upload_document_f(client, message):
 async def eval_message_f(client, message):
     status_message = await message.reply_text("Processing ...")
     cmd = message.text.split(" ", maxsplit=1)[1]
-
+ 
     reply_to_id = message.message_id
     if message.reply_to_message:
         reply_to_id = message.reply_to_message.message_id
-
+ 
     old_stderr = sys.stderr
     old_stdout = sys.stdout
     redirected_output = sys.stdout = io.StringIO()
     redirected_error = sys.stderr = io.StringIO()
     stdout, stderr, exc = None, None, None
-
+ 
     try:
         await aexec(cmd, client, message)
     except Exception:
         exc = traceback.format_exc()
-
+ 
     stdout = redirected_output.getvalue()
     stderr = redirected_error.getvalue()
     sys.stdout = old_stdout
     sys.stderr = old_stderr
-
+ 
     evaluation = ""
     if exc:
         evaluation = exc
@@ -188,12 +189,12 @@ async def eval_message_f(client, message):
         evaluation = stdout
     else:
         evaluation = "Success"
-
+ 
     final_output = "<b>EVAL</b>: <code>{}</code>\n\n<b>OUTPUT</b>:\n<code>{}</code> \n".format(
         cmd,
         evaluation.strip()
     )
-
+ 
     if len(final_output) > MAX_MESSAGE_LENGTH:
         with open("eval.text", "w+", encoding="utf8") as out_file:
             out_file.write(str(final_output))
@@ -207,8 +208,8 @@ async def eval_message_f(client, message):
         await status_message.delete()
     else:
         await status_message.edit(final_output)
-
-
+ 
+ 
 async def aexec(code, client, message):
     exec(
         f'async def __aexec(client, message): ' +
